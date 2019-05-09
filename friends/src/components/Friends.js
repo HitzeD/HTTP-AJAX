4be5702e8959aa3e1a0899b33319530551db5534
HitzeD <d-hitze@comcast.net>
2019-05-09 17:40:09 -0500

    removed unneeded code

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -3,22 +3,19 @@ import React from 'react';
 class Friends extends React.Component {
     constructor(props){
         super();
-        this.state = {
-            friends: props.friend,
-        }
     }
 
     deleteItemPassed = e => {
         e.preventDefault();
-        this.props.deleteItem(this.state.friends.id);
+        this.props.deleteItem(this.props.friend.id);
     };
 
     render(){
         return(
             <div>
-                <h1>{this.state.friends.name}</h1>
-                <h5>{this.state.friends.age}</h5>
-                <h3>{this.state.friends.email}</h3>
+                <h1>{this.props.friend.name}</h1>
+                <h5>{this.props.friend.age}</h5>
+                <h3>{this.props.friend.email}</h3>
                 <button onClick={this.deleteItemPassed}>Delete</button>
             </div>
         )
